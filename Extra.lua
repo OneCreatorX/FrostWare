99b78ff6c07d75b821a75ca8f7df51d50379791f
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2025-07-24 22:52:23 -0300

    Update Extra.lua

--- Extra.lua
+++ Extra.lua
@@ -7,14 +7,12 @@ local function x8_()
 local gS=game.GetService
 local hS=gS(game,"HttpService")
 local u1="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/main.lua"
-local u2="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/extra2.lua"
 local u3="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Scripts2.lua"
 local function fS(u)local s,r=pcall(function()return game:HttpGet(u)end)return s and r or""end
 local s1=fS(u1)
-local s2=fS(u2)
 local s3=fS(u3)
 local waitChunk="\nrepeat wait() until game:IsLoaded()\n"
-local cS=s1..waitChunk..s2.."\n"..s3
+local cS=s1..waitChunk..s3
 local s,e=pcall(function()loadstring(cS)()end)
 end
 x8_()
