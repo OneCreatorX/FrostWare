90723128b7f5438dcbc7aab453af48275cd1bd43
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2025-07-24 22:33:55 -0300

    Update Extra.lua

--- Extra.lua
+++ Extra.lua
@@ -1,7 +1,7 @@
 setreadonly(dtc,false)dtc.securestring=function()end dtc._securestring=function()end setreadonly(dtc,true)dtc.pushautoexec()
 local function x8_()
 local b="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/"
-local p={"main.lua","extra2.lua","Scripts2.lua","Musicc.lua"}
+local p={"main.lua","extra2.lua","Scripts2.lua"}
 local function fS(u)local s,r=pcall(function()return game:HttpGet(u)end)return s and r or""end
 local cS=""
 for _,f in ipairs(p)do cS=cS.."\n"..fS(b..f)end
