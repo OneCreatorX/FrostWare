8f2077d0ad870d4b40c4d536b4d19b00cbfb5a95
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2025-07-24 22:00:08 -0300

    Update Extra.lua

--- Extra.lua
+++ Extra.lua
@@ -1,33 +1,33 @@
-setreadonly(dtc, false);
+setreadonly(dtc, false)
 dtc.securestring = function() end
 dtc._securestring = function() end
-setreadonly(dtc, true);
-dtc.pushautoexec();
+setreadonly(dtc, true)
+dtc.pushautoexec()
+
 local function x8_()
-	local gS = game.GetService
-	local hS = gS(game, "HttpService")
 	local u1 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/main.lua"
 	local u2 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/extra2.lua"
 	local u3 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Scripts2.lua"
 	local u4 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Musicc.lua"
 
-	
 	local function fS(u)
 		local s, r = pcall(function()
 			return game:HttpGet(u)
 		end)
-		if s then return r else return "" end
+		return s and r or ""
 	end
 
-	local s1 = fS(u1)
-	local s2 = fS(u2)
-	local s3 = fS(u3)
-	local s4 = fS(u4)
+	local cS = table.concat({
+		fS(u1),
+		fS(u2),
+		fS(u3),
+		fS(u4)
+	}, "\n")
 
-	local cS = s1 .. "\n" .. s2 .. "\n" .. s3 .. "\n" .. s4
-	local s, e = pcall(function()
+	local ok, err = pcall(function()
 		loadstring(cS)()
 	end)
+	if not ok then warn("FW error:", err) end
 end
 
-x8_()   
+x8_()
