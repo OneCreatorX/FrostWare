5d50ccbaf86fe37b722a92addb51ae4967ec2109
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2025-07-24 23:19:56 -0300

    Update Extra.lua

--- Extra.lua
+++ Extra.lua
@@ -4,15 +4,15 @@ dtc._securestring=function()end
 setreadonly(dtc,true)
 dtc.pushautoexec()
 local function x8_()
-local gS=game.GetService
-local hS=gS(game,"HttpService")
-local u1="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/main.lua"
-local u3="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Scripts2.lua"
-local function fS(u)local s,r=pcall(function()return game:HttpGet(u)end)return s and r or""end
-local s1=fS(u1)
-local s3=fS(u3)
-local waitChunk="\nrepeat wait() until game:IsLoaded()\n"
-local cS=s1..waitChunk..s3
-local s,e=pcall(function()loadstring(cS)()end)
+    local gS=game.GetService
+    local fS=function(u)local s,r=pcall(function()return game:HttpGet(u)end)return s and r or""end
+    local u1="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/main.lua"
+    local u3="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Scripts2.lua"
+    local u4="https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Musicc.lua"
+    local s1=fS(u1)
+    local s2=fS(u3)
+    local s3=fS(u4)
+    local code=s1.."\n"..s2.."\n"..s3
+    local s,e=pcall(function()loadstring(code)()end)
 end
 x8_()
