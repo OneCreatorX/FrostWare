a5f840c9481e5f53a254e3572aaa674a82e73935
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2025-07-24 21:56:16 -0300

    Update Extra.lua

--- Extra.lua
+++ Extra.lua
@@ -9,7 +9,9 @@ local function x8_()
 	local u1 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/main.lua"
 	local u2 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/extra2.lua"
 	local u3 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Scripts2.lua"
+	local u4 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Musicc.lua"
 
+	
 	local function fS(u)
 		local s, r = pcall(function()
 			return game:HttpGet(u)
@@ -20,8 +22,9 @@ local function x8_()
 	local s1 = fS(u1)
 	local s2 = fS(u2)
 	local s3 = fS(u3)
+	local s4 = fS(u4)
 
-	local cS = s1 .. "\n" .. s2 .. "\n" .. s3
+	local cS = s1 .. "\n" .. s2 .. "\n" .. s3 .. "\n" .. s4
 	local s, e = pcall(function()
 		loadstring(cS)()
 	end)
