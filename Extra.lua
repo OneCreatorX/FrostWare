0898e58ee71a4dfcab1a94a27a056f40ff28f679
OneCreatorX <138682257+OneCreatorX@users.noreply.github.com>
2025-07-24 22:05:55 -0300

    Update Extra.lua

--- Extra.lua
+++ Extra.lua
@@ -1,39 +1,35 @@
-setreadonly(dtc, false)
+
+setreadonly(dtc, false);
 dtc.securestring = function() end
-dtc._securestring = function() end
-setreadonly(dtc, true)
-dtc.pushautoexec()
+dtc.securestring = function() end
+setreadonly(dtc, true);
+dtc.pushautoexec();
+local function x8()
+local gS = game.GetService
+local hS = gS(game, "HttpService")
+local u1 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/main.lua"
+local u2 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/extra2.lua"
+local u3 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Scripts2.lua"
+local u4 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Musicc.lua"
 
-local function x8_()
-    local gS = game.GetService
-    local hS = gS(game, "HttpService")
+local function fS(u)  
+	local s, r = pcall(function()  
+		return game:HttpGet(u)  
+	end)  
+	if s then return r else return "" end  
+end  
 
-    local u1 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/main.lua"
-    local u2 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/extra2.lua"
-    local u3 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Scripts2.lua"
-    local u4 = "https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Musicc.lua"
+local s1 = fS(u1)  
+local s2 = fS(u2)  
+local s3 = fS(u3)  
+local s4 = fS(u4)  
 
-    local function fS(u)
-        local s, r = pcall(function()
-            return game:HttpGet(u)
-        end)
-        return s and r or ""
-    end
+local cS = s1 .. "\n" .. s2 .. "\n" .. s3 .. "\n" .. s4  
+local s, e = pcall(function()  
+	loadstring(cS)()  
+end)
 
-    local cS = table.concat({
-        fS(u1),
-        fS(u2),
-        fS(u3),
-        fS(u4)
-    }, "\n")
-
-    local success, err = pcall(function()
-        loadstring(cS)()
-    end)
-
-    if not success then
-        warn("[FW] Error ", err)
-    end
 end
 
 x8_()
+
